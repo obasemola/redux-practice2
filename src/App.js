aaeb201085590fb8fa3be9ee4884922452e25d0f
obasemola <obasemolaayodele@gmail.com>
2020-10-08 21:22:11 +0300

    adding new anecdotes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
+import { getId } from './reducers/anecdoteReducer'
 
 const App = () => {
   const anecdotes = useSelector(state => state)
@@ -8,9 +9,27 @@ const App = () => {
   const vote = (id) => {
     return {
       type: 'INCREMENT',
-      id
-
     }
+  }
+
+  const addAnecdote = (anecdote) => {
+    return {
+      type: 'ADD',
+      data: {
+        content: anecdote,
+        id: getId(),
+        votes: 0
+      }
+    }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const anecdote = e.target.anecdote.value;
+    dispatch(addAnecdote(anecdote))
+    e.target.anecdote.value = ''
+    console.log(anecdote)
   }
 
 
@@ -30,8 +49,8 @@ const App = () => {
         </div>
       )}
       <h2>create new</h2>
-      <form>
-        <div><input /></div>
+      <form onSubmit={handleSubmit}>
+        <div><input name='anecdote'/></div>
         <button>create</button>
       </form>
     </div>
